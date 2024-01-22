c539121b10a632c54e4da1803dd013aae9ba42a6
Ekaterina Kulinich <kulinich.ekaterina.andreevna@gmail.com>
2024-01-22 18:28:43 +0300

    feat: prepares global styles

---
+++ src/components/Container.tsx
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+
+export const Container = styled.div`
+  margin: 0 auto;
+  padding: 0 15px;
+  width: 100%;
+  max-width: 1170px;
+  min-height: 100%;
+`
