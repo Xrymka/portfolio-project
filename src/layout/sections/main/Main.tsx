5082d88c2e0e09ee80b3b984ffd9c60710e4229b
Ekaterina Kulinich <kulinich.ekaterina.andreevna@gmail.com>
2024-01-24 01:17:49 +0300

    feat: styles main section

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,26 +1,36 @@
 import { styled } from 'styled-components';
 import main from '../../../assets/images/main.jpg'
 import { FlexWrapper } from '../../../components/FlexWrapper';
+import { Container } from '../../../components/Container';
+import { theme } from '../../../styles/Theme';
 
 export const Main = () => {
   return (
     <StyledMain>
-      <FlexWrapper align={"center"} justify={"space-around"}>
-        <div>
-            <span>Hi there</span>
-            <Name>I am Svetlana Dyablo</Name>
+      <Container>
+        <FlexWrapper align={"center"} justify={"space-between"}>
+          <div>
+            <SmallText>Hi there</SmallText>
+            <Name>I am <span>Svetlana Dyablo</span></Name>
             <MainTitle>A Web Developer.</MainTitle>
           </div>
 
-          <Photo src={main} alt="Svetlana's photo"/>
-      </FlexWrapper>
+          <PhotoWrapper>
+            <Photo src={main} alt="Svetlana's photo"/>
+          </PhotoWrapper>
+        </FlexWrapper>
+      </Container>
     </StyledMain>
   );
 };
 
 const StyledMain = styled.section`
+  display: flex;
   min-height: 100vh;
-  background-color: #ffffe0;
+
+  div {
+    text-align: left;
+  }
 `
 
 const Photo = styled.img`
@@ -29,9 +39,52 @@ const Photo = styled.img`
   object-fit: cover;
 `
 const MainTitle = styled.h1`
-  font-size: 20px;
+  font-size: 27px;
+  font-weight: 400;
 `
 
 const Name = styled.h2`
-  font-size: 30px;
+  margin: 10px 0;
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 50px;
+  font-weight: 700;
+  letter-spacing: .05em;
+
+  span {
+    position: relative;
+    z-index: 0;
+
+    &::after {
+      position: absolute;
+      right: 0;
+      bottom: 0;
+      content: "";
+      display: inline-block;
+      width: 100%;
+      height: 20px;
+      background-color: ${theme.colors.accent};
+      z-index: -1;
+    }
+  }
+`
+
+const SmallText = styled.span`
+  font-size: 14px;
+  font-weight: 400;
+`
+
+const PhotoWrapper = styled.div`
+  position: relative;
+  z-index: 0;
+
+  &::before {
+    position: absolute;
+    top: -24px;
+    left: 24px;
+    content: "";
+    width: 360px;
+    height: 470px;
+    border: 5px solid ${theme.colors.accent};
+    z-index: -1;
+  }
 `
--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -11,18 +11,18 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-  margin: 0;
-  font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: ${theme.colors.font};
+    margin: 0;
+    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: ${theme.colors.font};
+    line-height: 1.2;
   }
 
   code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-      monospace;
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
   }
 
   a {
